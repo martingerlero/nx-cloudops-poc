a24dbc1771a525f7ee62785ad721d219e9e428da
Martin Gerlero <martingerlero@gmail.com>
2025-06-25 17:40:08 +0000

    Final state import for all existing AWS resources

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -2,7 +2,7 @@ module "eks" {
   source          = "terraform-aws-modules/eks/aws"
   cluster_name    = "nx-poc-cluster"
   cluster_version = "1.29"
-  subnet_ids      = ["subnet-01035a3cc4e8c3aea", "subnet-0446eb40da96350f3", "subnet-0e387b6385a098d66", "subnet-0aea721faa5950885", "subnet-0c627f86faa525e81", "subnet-022ee25b627b01583"]
+  subnet_ids      = ["subnet-01035a3cc4e8c3aea", "subnet-0446eb40da96350f3", "subnet-0e387b6385a098d66", "subnet-0aea721faa5950885", "subnet-022ee25b627b01583"]
   vpc_id          = "vpc-057f7248d14054470"
   enable_irsa     = true
 
