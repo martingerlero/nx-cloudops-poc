3a468ea50418201be32f16a4618c259223207889
gerletin <martingerlero@gmail.com>
2025-06-25 10:35:21 -0300

    Create eks.tf

---
+++ terraform/eks.tf
@@ -0,0 +1,23 @@
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  cluster_name    = "nx-poc-cluster"
+  cluster_version = "1.29"
+  subnet_ids      = [/* tus subnets aquí */]
+  vpc_id          = "tu-vpc-id"
+  enable_irsa     = true
+
+  node_groups = {
+    default = {
+      desired_capacity = 1
+      max_capacity     = 1
+      min_capacity     = 1
+
+      instance_types = ["t3.micro"]
+      capacity_type  = "ON_DEMAND"
+    }
+  }
+
+  tags = {
+    Owner = "Martin Gerlero"
+  }
+}
